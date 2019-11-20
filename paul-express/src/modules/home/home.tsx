5ba5d87a79ba4acbc9b0ac1e86a4914984230a47
Sylvain Duhant <sylvain.duhant@nsi-sa.be>
2019-11-20 17:00:03 +0100

    Trying to make a home page ? looks like shit

--- paul-express/src/modules/home/home.tsx
+++ paul-express/src/modules/home/home.tsx
@@ -1,12 +1,51 @@
 import * as React from "react";
 import { SandwichCard } from "../../Components/SandwichCards";
+import styled from "styled-components";
+import { Button, Intent } from "@blueprintjs/core";
+import { iconClass, INTENT_PRIMARY } from "@blueprintjs/core/lib/esm/common/classes";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBicycle } from "@fortawesome/free-solid-svg-icons";
+import { StyledCard } from "nsitools-react";
 
 interface IHomeProps {}
+//TODO: improve this shit plz
+//looks like shit atm, need to style the buttons and change flex
+
+const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ImgContainer = styled.div`
+  display: flex;
+  flex-direction: Column;
+  flex: 0.05;
+  align-self: center;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  flex: 1;
+  flex-grow: 1;
+`;
 
 export const Home: React.FunctionComponent<IHomeProps> = props => {
   return (
-    <SandwichCard
-      sandwich={{ name: "Frenchy", price: 3.8, ingredients: ["Brie", "Beurre", "Noix", "Confit de figue"] }}
-    />
+    <Container>
+      <ImgContainer>
+        <div>
+          <img src={require("../../Ressources/Images/logo_v1.png")} alt="Paul Express's logo" />
+        </div>
+      </ImgContainer>
+      <ButtonContainer>
+        <Button intent={Intent.PRIMARY} icon={<FontAwesomeIcon icon={faBicycle} />} text="Commander"></Button>
+        <Button intent={Intent.PRIMARY} text="Commandes antérieures"></Button>
+        <Button intent={Intent.PRIMARY} text="Comande en cours"></Button>
+      </ButtonContainer>
+    </Container>
   );
 };

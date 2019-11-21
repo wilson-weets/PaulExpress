67e9117cdda6c0eca0c30779d5bf6f038c533ac7
Wilson Weets <wilson.weets@nsi-sa.be>
2019-11-21 12:29:15 +0100

    Added base entity

---
+++ PaulExpress.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaulExpress.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}

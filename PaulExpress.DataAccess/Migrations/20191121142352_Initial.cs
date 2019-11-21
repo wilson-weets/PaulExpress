3dcf764f11b97aa6678c29e25e1b3e9ad15fe982
Wilson Weets <wilson.weets@nsi-sa.be>
2019-11-21 15:28:20 +0100

    Added initial migration + updated database

---
+++ PaulExpress.DataAccess/Migrations/20191121142352_Initial.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaulExpress.DataAccess.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(nullable: true),
+                    UpdatedOn = table.Column<DateTime>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    Comment = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PaymentMethods",
+                columns: table => new
+                {
+                    PaymentMethodId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(nullable: true),
+                    UpdatedOn = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentMethods", x => x.PaymentMethodId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sandwiches",
+                columns: table => new
+                {
+                    SandwichId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(nullable: true),
+                    UpdatedOn = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Ingredients = table.Column<string>(nullable: true),
+                    UnitPrice = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sandwiches", x => x.SandwichId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sauces",
+                columns: table => new
+                {
+                    SauceId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(nullable: true),
+                    UpdatedOn = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sauces", x => x.SauceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Supplements",
+                columns: table => new
+                {
+                    SupplementId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(nullable: true),
+                    UpdatedOn = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<int>(nullable: false),
+                    UnitPrice = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Supplements", x => x.SupplementId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(nullable: true),
+                    UpdatedOn = table.Column<DateTime>(nullable: false),
+                    Trigram = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderLines",
+                columns: table => new
+                {
+                    OrderLineId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(nullable: true),
+                    UpdatedOn = table.Column<DateTime>(nullable: false),
+                    UserId = table.Column<int>(nullable: false),
+                    SandwichId = table.Column<int>(nullable: false),
+                    SauceId = table.Column<int>(nullable: true),
+                    OrderLineSupplementId = table.Column<int>(nullable: true),
+                    PaymentMethodId = table.Column<int>(nullable: false),
+                    OrderId = table.Column<int>(nullable: false),
+                    Comment = table.Column<string>(nullable: true),
+                    IsPayed = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderLines", x => x.OrderLineId);
+                    table.ForeignKey(
+                        name: "FK_OrderLines_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderLines_PaymentMethods_PaymentMethodId",
+                        column: x => x.PaymentMethodId,
+                        principalTable: "PaymentMethods",
+                        principalColumn: "PaymentMethodId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderLines_Sandwiches_SandwichId",
+                        column: x => x.SandwichId,
+                        principalTable: "Sandwiches",
+                        principalColumn: "SandwichId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderLines_Sauces_SauceId",
+                        column: x => x.SauceId,
+                        principalTable: "Sauces",
+                        principalColumn: "SauceId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_OrderLines_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderLineSupplements",
+                columns: table => new
+                {
+                    OrderLineSupplementId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<string>(nullable: true),
+                    UpdatedOn = table.Column<DateTime>(nullable: false),
+                    OrderLineId = table.Column<int>(nullable: false),
+                    SupplementId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderLineSupplements", x => x.OrderLineSupplementId);
+                    table.ForeignKey(
+                        name: "FK_OrderLineSupplements_OrderLines_OrderLineId",
+                        column: x => x.OrderLineId,
+                        principalTable: "OrderLines",
+                        principalColumn: "OrderLineId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderLineSupplements_Supplements_SupplementId",
+                        column: x => x.SupplementId,
+                        principalTable: "Supplements",
+                        principalColumn: "SupplementId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLines_OrderId",
+                table: "OrderLines",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLines_PaymentMethodId",
+                table: "OrderLines",
+                column: "PaymentMethodId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLines_SandwichId",
+                table: "OrderLines",
+                column: "SandwichId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLines_SauceId",
+                table: "OrderLines",
+                column: "SauceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLines_UserId",
+                table: "OrderLines",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLineSupplements_OrderLineId",
+                table: "OrderLineSupplements",
+                column: "OrderLineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLineSupplements_SupplementId",
+                table: "OrderLineSupplements",
+                column: "SupplementId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderLineSupplements");
+
+            migrationBuilder.DropTable(
+                name: "OrderLines");
+
+            migrationBuilder.DropTable(
+                name: "Supplements");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "PaymentMethods");
+
+            migrationBuilder.DropTable(
+                name: "Sandwiches");
+
+            migrationBuilder.DropTable(
+                name: "Sauces");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}

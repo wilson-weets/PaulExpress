a9f2f7192921e19826fbd339b43971ab4d5c7a5d
Wilson Weets <wilson.weets@nsi-sa.be>
2019-11-22 10:14:58 +0100

    Created first repositories (wip)

---
+++ PaulExpress.Abstractions/Repositories/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using NsiTools.EfUtils.Core.Interfaces;
+using PaulExpress.Domain.Entities;
+
+namespace PaulExpress.Abstractions.Repositories
+{
+    public interface IOrderRepository: IGenericRepository<Order>
+    {
+        
+    }
+}

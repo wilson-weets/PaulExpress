766d24d798ce3f4443df0a6184a116c3fea24139
Michaux Chloé <michauxchloe@hotmail.fr>
2019-11-29 12:18:51 +0100

    Migration

---
+++ Doc/Scripts/InitialData.sql
@@ -0,0 +1,54 @@
+-- Shop
+INSERT INTO Shop (Name, ShopId)
+VALUES ("", 1);
+
+-- Sauces
+
+-- Supplements
+
+-- PaymentMethod
+INSERT INTO PaymentMethod(Name)
+VALUES("Cash")
+INSERT INTO PaymentMethod(Name)
+VALUES("Virement")
+
+--Sandwich
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le Club", "Spécialités", "Jambon, fromage, oeuf, tomate, carotte, salade, mayo", 3.5)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le Club spécal", "Spécialités", "Club avec sauce andalouse", 3.5)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le Rustachou", "Spécialités", "Filet de poulet, emmental, oeuf, tomate, salade, andalouse", 3.5)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le Club med", "Spécialités", "Chorizo, emmental, oeur, tomate, carotte, salade, mayo", 3.5)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le Traditionnel", "Spécialités", "Pesto vert, jambon italien, tomate, mozzarella, roquette", 3.5)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le St-Tropez", "Spécialités", "Jambon, fromage, oeuf, tomate, carotte, salade, mayo", 3.5)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le Turkey", "Spécialités", "Jambon, fromage, oeuf, tomate, carotte, salade, mayo", 3.5)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le British", "Spécialités", "Jambon, fromage, oeuf, tomate, carotte, salade, mayo", 3.5)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "L' U.S.A", "Spécialités", "Jambon, fromage, oeuf, tomate, carotte, salade, mayo", 3.5)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le Chicago", "Spécialités", "Jambon, fromage, oeuf, tomate, carotte, salade, mayo", 3.5)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le Tyrolien", "Spécialités", "Jambon, fromage, oeuf, tomate, carotte, salade, mayo", 3.5)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le Suisse", "Spécialités", "Jambon, fromage, oeuf, tomate, carotte, salade, mayo", 3.5)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le Normand", "Spécialités", "Jambon, fromage, oeuf, tomate, carotte, salade, mayo", 3.5)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le Liégeois", "Spécialités", "Jambon, fromage, oeuf, tomate, carotte, salade, mayo", 3.5)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le Délice", "Spécialités", "Jambon, fromage, oeuf, tomate, carotte, salade, mayo", 3.5)
+
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le Rital", "Spécialités", "Jambon, fromage, oeuf, tomate, carotte, salade, mayo", 3.8)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le St-Hubert", "Spécialités", "Jambon, fromage, oeuf, tomate, carotte, salade, mayo", 3.8)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le Crête", "Spécialités", "Jambon, fromage, oeuf, tomate, carotte, salade, mayo", 3.8)
+INSERT INTO Sandwich(ShopId, Name, Categorie, Ingredients, UnitPrice)
+VALUES(1, "Le Fresh", "Spécialités", "Jambon, fromage, oeuf, tomate, carotte, salade, mayo", 3.8)
---
+++ PaulExpress.DataAccess/Migrations/20191129103057_Update_Sauce_Supplement_tables.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaulExpress.DataAccess.Migrations
+{
+    public partial class Update_Sauce_Supplement_tables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ShopId",
+                table: "Supplements",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ShopId",
+                table: "Sauces",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShopId",
+                table: "Supplements");
+
+            migrationBuilder.DropColumn(
+                name: "ShopId",
+                table: "Sauces");
+        }
+    }
+}

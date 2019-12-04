3d00fdfa505ec4d76c75070340af7e761a3fca4d
Sylvain Duhant <sylvain.duhant@nsi-sa.be>
2019-12-04 14:36:28 +0100

    Fixed Typo (sandiwch)

--- PaulExpress.Frontend/src/AppRouter.tsx
+++ PaulExpress.Frontend/src/AppRouter.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import { SandiwchList } from "./modules/sandwich/SandwichList";
+import { SandwichList } from "./modules/sandwich/SandwichList";
 import { Home } from "./modules/Home";
 
 export enum ERoutes {
@@ -12,7 +12,7 @@ export const AppRouter = () => {
   return (
     <Switch>
       <Route path={`${ERoutes.home}`} component={Home}></Route>
-      <Route path={`${ERoutes.sandwichList}/:shopId`} component={SandiwchList}></Route>
+      <Route path={`${ERoutes.sandwichList}/:shopId`} component={SandwichList}></Route>
       <Redirect to={ERoutes.home} exact />
     </Switch>
   );
--- PaulExpress.Frontend/src/modules/sandwich/SandwichList.tsx
+++ PaulExpress.Frontend/src/modules/sandwich/SandwichList.tsx
@@ -2,14 +2,14 @@ import * as React from "react";
 import useReactRouter from "use-react-router";
 import { SandwichCard } from "./SandwichCard";
 
-interface ISandiwchListProps {}
+interface ISandwichListProps {}
 
-export const SandiwchList: React.FunctionComponent<ISandiwchListProps> = props => {
+export const SandwichList: React.FunctionComponent<ISandwichListProps> = props => {
   const { match } = useReactRouter<{ shopId: string }>();
   const shopId = React.useMemo(() => +match.params.shopId, [match.params.shopId]);
   return (
     <div>
-      <SandwichCard sandwich={{}}></SandwichCard>SandiwchList works! (shopId = {shopId})
+      <SandwichCard sandwich={{}}></SandwichCard>SandwichList works! (shopId = {shopId})
     </div>
   );
 };

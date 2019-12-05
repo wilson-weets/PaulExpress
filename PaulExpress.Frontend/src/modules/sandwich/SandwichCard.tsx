e24d66f6927096a08bcbc86fa0c4da862ce23c9b
Sylvain Duhant <sylvain.duhant@nsi-sa.be>
2019-12-05 10:12:59 +0100

    Adding hard coded sandwiches, and styling sandich list and cards

--- PaulExpress.Frontend/src/modules/sandwich/SandwichCard.tsx
+++ PaulExpress.Frontend/src/modules/sandwich/SandwichCard.tsx
@@ -1,15 +1,36 @@
 import * as React from "react";
 import { SandwichDto } from "../../api/models/SandwichDto";
 import styled from "styled-components";
+import { SelectBox, CheckboxInputField } from "nsitools-react";
+import { Checkbox } from "@blueprintjs/core";
 
 interface ISandwichCardProps {
   sandwich: SandwichDto;
+  style?: React.CSSProperties;
 }
 
 const Container = styled.div`
   display: flex;
+  flex-direction: Row;
+  justify-content: space-between;
+  border: solid darkgrey 1px;
+  border-radius: 5px;
+  padding: 0.5rem;
+  background-color: white;
+  min-width: 200px;
 `;
 
-export const SandwichCard: React.FunctionComponent<ISandwichCardProps> = ({ sandwich }) => {
-  return <Container>SandwichCard works!</Container>;
+export const SandwichCard: React.FunctionComponent<ISandwichCardProps> = ({ sandwich, style }) => {
+  return (
+    <Container style={style}>
+      <div>
+        <div>{sandwich!.name}</div>
+        <div style={{ paddingTop: "0.5rem" }}>{sandwich!.ingredients}</div>
+      </div>
+      <div>
+        <div>{sandwich.unitPrice && sandwich.unitPrice + "€"}</div>
+        <Checkbox style={{ float: "right", paddingTop: "0.5rem" }} />
+      </div>
+    </Container>
+  );
 };
--- PaulExpress.Frontend/src/modules/sandwich/SandwichList.tsx
+++ PaulExpress.Frontend/src/modules/sandwich/SandwichList.tsx
@@ -1,15 +1,80 @@
 import * as React from "react";
 import useReactRouter from "use-react-router";
 import { SandwichCard } from "./SandwichCard";
+import styled from "styled-components";
+import { Colors } from "@blueprintjs/core";
 
 interface ISandwichListProps {}
 
 export const SandwichList: React.FunctionComponent<ISandwichListProps> = props => {
+  const Container = styled.div<{ isDark: boolean }>`
+    display: flex;
+    flex-direction: Row;
+    border: solid black 1px;
+    border-radius: 7px;
+    padding: 0.5rem;
+    background-color: ${p => (p.isDark ? Colors.DARK_GRAY4 : Colors.LIGHT_GRAY5)};
+    min-width: 200px;
+    flex-wrap: wrap;
+  `;
+
   const { match } = useReactRouter<{ shopId: string }>();
   const shopId = React.useMemo(() => +match.params.shopId, [match.params.shopId]);
   return (
-    <div>
-      <SandwichCard sandwich={{}}></SandwichCard>SandwichList works! (shopId = {shopId})
-    </div>
+    <Container isDark={false}>
+      <div style={{ padding: "0.5rem" }}>
+        <SandwichCard
+          sandwich={{ name: "HardCodedSandwich", ingredients: "patates patates", unitPrice: 120 }}
+        ></SandwichCard>
+      </div>
+      <div style={{ padding: "0.5rem" }}>
+        <SandwichCard
+          sandwich={{ name: "HardCodedSandwich", ingredients: "patates patates", unitPrice: 120 }}
+        ></SandwichCard>
+      </div>
+      <div style={{ padding: "0.5rem" }}>
+        <SandwichCard
+          sandwich={{ name: "HardCodedSandwich", ingredients: "patates patates", unitPrice: 120 }}
+        ></SandwichCard>
+      </div>
+      <div style={{ padding: "0.5rem" }}>
+        <SandwichCard
+          sandwich={{ name: "HardCodedSandwich", ingredients: "patates patates", unitPrice: 120 }}
+        ></SandwichCard>
+      </div>
+      <div style={{ padding: "0.5rem" }}>
+        <SandwichCard
+          sandwich={{ name: "HardCodedSandwich", ingredients: "patates patates", unitPrice: 120 }}
+        ></SandwichCard>
+      </div>
+      <div style={{ padding: "0.5rem" }}>
+        <SandwichCard
+          sandwich={{ name: "HardCodedSandwich", ingredients: "patates patates", unitPrice: 120 }}
+        ></SandwichCard>
+      </div>
+      <div style={{ padding: "0.5rem" }}>
+        <SandwichCard
+          sandwich={{ name: "HardCodedSandwich", ingredients: "patates patates", unitPrice: 120 }}
+        ></SandwichCard>
+      </div>
+
+      <div style={{ padding: "0.5rem" }}>
+        <SandwichCard
+          sandwich={{ name: "HardCodedSandwich", ingredients: "patates patates", unitPrice: 120 }}
+        ></SandwichCard>
+      </div>
+
+      <div style={{ padding: "0.5rem" }}>
+        <SandwichCard
+          sandwich={{ name: "HardCodedSandwich", ingredients: "patates patates", unitPrice: 120 }}
+        ></SandwichCard>
+      </div>
+
+      <div style={{ padding: "0.5rem" }}>
+        <SandwichCard
+          sandwich={{ name: "HardCodedSandwich", ingredients: "patates patates", unitPrice: 120 }}
+        ></SandwichCard>
+      </div>
+    </Container>
   );
 };
